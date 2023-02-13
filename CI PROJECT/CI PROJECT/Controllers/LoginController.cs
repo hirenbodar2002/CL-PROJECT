c6e0f771574ebef5a75ff54e00317d3267f653f0
hiren bodar <hirenbodar318@gmail.com>
2023-02-13 15:54:45 +0530

    LOGIN

---
+++ CI PROJECT/CI PROJECT/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CI_PROJECT.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Forgotpassword()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult newpassword()
+        {
+            return View();
+        }
+    }
+}
