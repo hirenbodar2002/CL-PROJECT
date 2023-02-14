4e2f1f8b6681ac3837b194377a4ba3eb8622fb1a
hiren bodar <hirenbodar318@gmail.com>
2023-02-14 16:59:33 +0530

    new password

--- CI PROJECT/CI PROJECT/Controllers/LoginController.cs
+++ CI PROJECT/CI PROJECT/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CI_PROJECT.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CI_PROJECT.Controllers
 {
@@ -13,6 +14,11 @@ namespace CI_PROJECT.Controllers
             return View();
         }
         public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(RegisterModel model)
         {
             return View();
         }
---
+++ CI PROJECT/CI PROJECT/Models/RegisterModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CI_PROJECT.Models
+{ 
+    public class RegisterModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Firstname { get; set; }
+        public string Lastname { get; set; }
+        public long phonenumber { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string confrimPassword { get; set; }
+
+
+    }
+}
